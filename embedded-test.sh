d6e4bb231ea564190a1ea546817affbce3b8e7d5
Waldemar Brodkorb <wbx@openadk.org>
2016-12-13 03:08:28 +0100

    add sparc64 newlib toolchain tests

--- embedded-test.sh
+++ embedded-test.sh
@@ -48,7 +48,7 @@ arch_list_glibc="aarch64 aarch64be alpha armv5 armv7 armeb hppa \
 arch_list_newlib="aarch64 aarch64be arcv1 armv5 armeb bfin crisv10 \
   crisv32 epiphany frv h8300 lm32 m32r m68k microblazeel \
   microblazebe mips mipsel mn10300 moxie msp430 nds32le nds32be \
-  nios2 or1k ppc sparc v850 x86"
+  nios2 or1k ppc sparc sparc64 v850 x86"
 
 topdir=$(pwd)
 giturl=http://git.openadk.org/openadk.git
@@ -889,10 +889,11 @@ get_arch_info() {
       suffix=${cpu_arch}
       ;;
     sparc64)
-      allowed_libc="glibc"
+      allowed_libc="glibc newlib"
       runtime_test="glibc"
       allowed_tests="toolchain boot libc mksh ltp native"
       default_glibc="ADK_APPLIANCE=test ADK_TARGET_OS=linux ADK_TARGET_ARCH=sparc64 ADK_TARGET_FS=initramfsarchive ADK_TARGET_SYSTEM=qemu-sparc64"
+      default_newlib="ADK_APPLIANCE=toolchain ADK_TARGET_OS=baremetal ADK_TARGET_ARCH=sparc64"
       cpu_arch=v9
       qemu=qemu-system-sparc64
       qemu_machine=sun4u
