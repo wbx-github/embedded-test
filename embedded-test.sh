7fd1c84e3d2e852aefb4d303baf033fa3d777779
Waldemar Brodkorb <wbx@openadk.org>
2023-09-07 11:39:25 +0200

    add test for ARM FDPIC w/o NPTL

--- embedded-test.sh
+++ embedded-test.sh
@@ -24,7 +24,7 @@
 
 # uClibc-ng
 arch_list_uclibcng="aarch64 aarch64be alpha arcv2 \
-  armv5 armv5-nommu-arm armv5-nommu-thumb armv6 armv7 \
+  armv5 armv5-nommu-arm armv5-nommu-thumb armv5-nommu-fdpic armv6 armv7 \
   armv7-thumb2 armv8 armv8-thumb2 armeb avr32 \
   bf512-flat bf512-fdpic bf532-flat bf532-fdpic \
   crisv10 crisv32 csky-ck807 csky-ck810 \
@@ -314,6 +314,20 @@ get_arch_info() {
       qemu=qemu-system-arm
       qemu_machine=versatilepb
       suffix=${cpu_arch}_soft_eabi_flat_thumb_nommu
+      dtbdir=openadk/firmware/qemu-${march}_${lib}_${suffix}
+      qemu_args="${qemu_args} -m 256 -cpu arm926 -net user -net nic,model=smc91c111 -dtb ${dtbdir}/versatile-pb.dtb"
+      piggyback=1
+      ;;
+    armv5-nommu-fdpic)
+      allowed_libc="uclibc-ng"
+      runtime_test="uclibc-ng"
+      allowed_tests="toolchain boot libc"
+      default_uclibc_ng="ADK_APPLIANCE=test ADK_TARGET_OS=$os ADK_TARGET_ARCH=arm ADK_TARGET_FS=initramfspiggyback ADK_TARGET_SYSTEM=qemu-arm-versatilepb ADK_TARGET_ENDIAN=little ADK_TARGET_MMU=no ADK_TARGET_INSTRUCTION_SET=arm ADK_TARGET_BINFMT=fdpic ADK_TARGET_THREADS=none ADK_TARGET_FLOAT=soft"
+      cpu_arch=arm926ej_s
+      march=arm-versatilepb
+      qemu=qemu-system-arm
+      qemu_machine=versatilepb
+      suffix=${cpu_arch}_soft_eabi_fdpic_arm_nommu
       dtbdir=openadk/firmware/qemu-${march}_${lib}_${suffix}
       qemu_args="${qemu_args} -m 256 -cpu arm926 -net user -net nic,model=smc91c111 -dtb ${dtbdir}/versatile-pb.dtb"
       piggyback=1
