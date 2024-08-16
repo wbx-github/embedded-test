843fca7479f5b465218c91076ef1b5e38bd6a103
Waldemar Brodkorb <wbx@openadk.org>
2024-08-16 12:57:22 +0200

    update uclibc-ng to 1.0.50

--- embedded-test.sh
+++ embedded-test.sh
@@ -1690,7 +1690,7 @@ for lib in ${libc}; do
       if [[ $libcversion ]]; then
         version=$libcversion
       else
-        version=1.0.49
+        version=1.0.50
       fi
       libver=uClibc-ng-${version}
       libdir=uClibc-ng
